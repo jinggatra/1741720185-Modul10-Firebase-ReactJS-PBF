0946f6d9dc8657f689fc130063d51ff3da92698b
jinggatra <wmkbisa@gmail.com>
2020-04-16 12:28:29 +0700

    firbase.config.js

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+export default{
+    apiKey: "AIzaSyBDvt-_L5L3lDalhlvwXLUdcHJox5Ruol8",
+    authDomain: "fir-reactjs-4469e.firebaseapp.com",
+    databaseURL: "https://fir-reactjs-4469e.firebaseio.com",
+    projectId: "fir-reactjs-4469e",
+    storageBucket: "fir-reactjs-4469e.appspot.com",
+    messagingSenderId: "812133458488",
+    appId: "1:812133458488:web:34722af035b204a5987ccd",
+    measurementId: "G-6JHDTD34SE"
+};
+
